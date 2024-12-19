0fab165992269eee47e7dbab9965b990bab19c7e
derek-kuang <derekk7966@gmail.com>
2024-12-19 10:48:08 -0800

    Update apiexample.js

--- apiexample.js
+++ apiexample.js
@@ -16,7 +16,7 @@ window.onload = function() {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('sw.js').then(function(registration) {
+    navigator.serviceWorker.register('tvmaze1/sw.js').then(function(registration) {
       console.log('Service Worker registered with scope:', registration.scope);
     }, function(error) {
       console.log('Service Worker registration failed:', error);
