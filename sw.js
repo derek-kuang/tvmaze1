a532148ad9b551d4cbca0f58a3cf14e2d82b435a
derek-kuang <derekk7966@gmail.com>
2024-12-19 10:22:17 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,86 @@
+const version = 'v125';  // change this everytime you update the service worker
+                          // to force the browser to also update it.
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'style.css',
+        'apiexample.js',
+        'icons/icon512_rounded.png',
+        'icons/icon512_maskable.png',
+        'tv.jpg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Define cache names
+const CACHE_NAME = 'my-app-cache-v1';
+const DYNAMIC_CACHE_NAME = 'my-app-dynamic-cache-v1';
+
+// Files to cache during install
+const STATIC_ASSETS = [
+  '/', 
+  '/index.html',
+  '/style.css',
+  '/apiexample.js',
+  'icons/icon512_rounded.png',        
+  'icons/icon512_maskable.png',
+  'tv.jpg'
+  // Add other static files you want to cache
+];
+
+// Install event: Cache static assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+});
+
+// Activate event: Clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME && cache !== DYNAMIC_CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event: Network-first strategy
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(networkResponse => {
+        // If network fetch is successful, cache the response
+        return caches.open(DYNAMIC_CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => {
+        // If network fetch fails, fallback to cache
+
+        return caches.match(event.request);
+      })
+  );
+});
